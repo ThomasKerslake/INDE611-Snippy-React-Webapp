a81f0ac650cb9c3f4df29f5cc9e2ec1d70e9394b
ThomasKerslake <33869524+ThomasKerslake@users.noreply.github.com>
2020-04-17 19:06:03 +0100

    Snippy-functions, get and post

---
+++ snippy-functions/dbschema.js
@@ -0,0 +1,12 @@
+//database schema
+let db = {
+  snips: [
+    {
+      userHandle: "userName",
+      body: "this is some body of text",
+      createdAt: "2020-04-15T11:46:01.018Z",
+      numberOfLikes: 7,
+      numberOfComments: 3,
+    },
+  ],
+};
